bec7e646255a8d53f02c35aadeea86faac9c91e0
Jules BARBIER <jubarbie@e3r2p1.42.fr>
2018-04-27 20:52:35 +0200

    playing with va

--- Includes/ft_printf.h
+++ Includes/ft_printf.h
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubarbie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/27 18:18:31 by jubarbie          #+#    #+#             */
+/*   Updated: 2018/04/27 18:23:18 by jubarbie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdarg.h> 
 # include "libft.h"
 
 int	ft_printf(const char * restrict format, ...);
--- Sources/ft_printf.c
+++ Sources/ft_printf.c
@@ -1,10 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubarbie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/27 18:23:41 by jubarbie          #+#    #+#             */
+/*   Updated: 2018/04/27 18:54:16 by jubarbie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
+
+int	parse_input(char *str)
+{
+	int	i;
+	int	nb_args;
+
+	nb_args = 0;
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == '%')
+			nb_args++;
+	}
+	return nb_args;
+}
 
 int	ft_printf(const char * restrict format, ...)
 {
-	int	ret;
-	
-	ret = ft_strlen(format);
-	ft_putstr(format);
-	return (ret);
+	int		nb_args;
+	int		max;
+	va_list	ap;
+
+	nb_args = parse_input(format);
+	va_start(ap, nb_args);
+	max = va_arg(ap, int)
+	return (0);
 }
